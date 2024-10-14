a52d5d5eb26c0514bf0f7d6f23a0cd75df2b849b
Abinaya139 <abinayashree293@gmail.com>
2024-10-14 21:43:16 +0530

    Update script.js

--- food delivery/script.js
+++ food delivery/script.js
@@ -13,7 +13,7 @@ const restaurantMenus = {
     ],
     'Bread & Spread': [
         { name: 'Cheeseburger', price: 9.99, img: food delivery/images/cheese_burger.jpg},
-        { name: 'Fries', price: 3.99, img: "E:\abi fswd\fries.jpg" }
+        { name: 'Fries', price: 3.99, img: food delivery/images/fries.jpg }
     ]
 };
 
